9a376a346c6d20a37fb3a0a3f06ac66422dc6e12
Davi <davilimasoares1@outlook.com>
2025-11-27 10:42:12 -0300

    first try

---
+++ script.js
@@ -0,0 +1,7 @@
+let = defaultGrid = 16;
+
+const pixel = document.createElement('div')
+const container = document.querySelector('#container');
+
+container.appendChild(pixel);
+pixel.classList.add('pixel');
