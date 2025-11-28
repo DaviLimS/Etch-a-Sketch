b7e5d4ae4f284f0f4ca96f587e2dc01bc0017c90
Davi <davilimasoares1@outlook.com>
2025-11-28 09:29:59 -0300

    I created the hover effect and the colorfu mode

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let = gridSize = 16;
 
 function createGrid(size) {
-    let color;
+}
+
+function createColorful(size) {
     const container = document.querySelector('#container');
 
     function colorSet() {
-        let r = Math.round(Math.random() * 100);
-        let g = Math.round(Math.random() * 100);
-        let b = Math.round(Math.random() * 100);
-        color = `rgb: "${r}, ${g}, ${b}"`;
+        let r = Math.round(Math.random() * 256);
+        let g = Math.round(Math.random() * 256);
+        let b = Math.round(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
     }
-    colorSet();
 
     container.innerHTML = '';
     container.style.setProperty('--grid-size', size)
-    container.style.setProperty('--color', color)
 
     for (let i =0; i < size * size; i++) {
         const pixel = document.createElement('div')
         container.appendChild(pixel);
         pixel.classList.add('pixel');
         pixel.addEventListener('mouseover', () => {
-        pixel.style.backgroundColor = color;
-    })
+            pixel.style.backgroundColor = colorSet();
+        })
     }
 }
 
